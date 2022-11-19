3a4bd5e9c7bdbbe1fa1d40e1e63e68734782fff4
Pratik Devle <pratik.devle98@gmail.com>
2022-11-19 21:28:29 +0530

    feat: added get, post and delete apis

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,51 @@
+import axios from "axios";
 import {useState, useEffect} from "react";
 
 const Home = () => {
   const [inputText, setInputText] = useState("");
   const [list, setList] = useState([]);
+  const [isTextAdded, setIsTextAdded] = useState(false);
 
-  //   useEffect(() => {
-
-  //   }, [])
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get("/api/todos");
+        console.log(response);
+        setList(response.data.todos);
+      } catch (error) {
+        console.error("get api", error);
+      }
+    })();
+  }, [isTextAdded]);
 
   const handleToDo = () => {
-    setList((prev) => [...prev, inputText]);
+    (async () => {
+      try {
+        const response = await axios.post(
+          "/api/todos",
+          {text: inputText},
+          {headers: {}}
+        );
+        console.log(response);
+      } catch (error) {
+        console.error("post todo", error);
+      }
+    })();
     setInputText("");
+    setIsTextAdded((prev) => !prev);
   };
+
+  const handleDelete = (id) => {
+    (async () => {
+      try {
+        const response = await axios.delete(`/api/todos/${id}`);
+        setIsTextAdded((prev) => !prev);
+      } catch (error) {
+        console.error("delete todo", error);
+      }
+    })();
+  };
+
   return (
     <div>
       <h1>MirageJs To-Do App</h1>
@@ -26,8 +60,11 @@ const Home = () => {
       <div>
         <h2>All To-Do:</h2>
         <ul>
-          {list.map((item, index) => (
-            <li key={index}>{item}</li>
+          {list.map((item) => (
+            <li key={item.id}>
+              {item.text}{" "}
+              <button onClick={() => handleDelete(item.id)}>X</button>
+            </li>
           ))}
         </ul>
       </div>
--- src/server.js
+++ src/server.js
@@ -1,15 +1,32 @@
-import {createServer} from "miragejs";
+import {createServer, Model} from "miragejs";
 
 export default function () {
   createServer({
+    models: {
+      todo: Model,
+    },
+
+    seeds(server) {
+      server.create("todo", {text: "Learn MirageJs"});
+      server.create("todo", {text: "Workout"});
+      server.create("todo", {text: "Learn JS"});
+    },
+
     routes() {
-      this.get("/api/todo", () => ({
-        todos: [
-          {id: 1, text: "Learn MirageJs"},
-          {id: 2, text: "Clean floor"},
-          {id: 3, text: "Workout"},
-        ],
-      }));
+      this.get("/api/todos", (schema) => {
+        return schema.todos.all();
+      });
+
+      this.post("/api/todos", (schema, request) => {
+        let data = JSON.parse(request.requestBody);
+        console.log(data);
+        return schema.todos.create(data);
+      });
+
+      this.delete("/api/todos/:id", (schema, request) => {
+        let todoId = request.params.id;
+        return schema.todos.find(todoId).destroy();
+      });
     },
   });
 }
